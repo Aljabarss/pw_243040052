b5a10b5cd1caee81bbc6650b84669a61249d5170
Aljabarss <aljabarshaleh21@gmail.com>
2025-02-17 19:24:09 +0700

    Add files via upload

---
+++ kuliah/pertemuan2/latihan2.php
@@ -0,0 +1,13 @@
+<?php
+
+$namalengkap = "Aljabar Shaleh Muhammad" ;
+$email = "aljabarshaleh21@gmail.com" ;
+$jurusan = "Teknik Informatika" ;
+$universitas = "UNPAS" ;
+$hobi = "Bermain Game" ;
+
+echo "<h1>$namaLengkap</h1>" ;
+echo "<h4>$email</h4>" ;
+echo "<p>Hallo, nama saya $namaLengkap" ;
+
+?>
